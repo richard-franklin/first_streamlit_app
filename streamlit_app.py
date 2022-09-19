0cea279e19a545ddf0dc5ef231dbcbae860fc8ec
D Richard Franklin <53473010+richard-franklin@users.noreply.github.com>
2022-09-19 15:30:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as sl
 sl.title("My Parents New Healthy Diner")
 
 sl.header("Breakfast Menu")
-sl.text("Omega 3 & Blueberry Oatmeal")
-sl.text("Kale, Spinach & Rocket Smoothie")
-sl.text("Hard-Boiled Free-Range Egg")
+sl.text("🥣 Omega 3 & Blueberry Oatmeal")
+sl.text("🥗 Kale, Spinach & Rocket Smoothie")
+sl.text("🐔 Hard-Boiled Free-Range Egg")
+sl.text("🥑🍞 Avocado Toast")
