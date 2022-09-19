f81357f6b4497e151723236ce57e5223e198b6d0
D Richard Franklin <53473010+richard-franklin@users.noreply.github.com>
2022-09-19 15:30:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ sl.text("🥣 Omega 3 & Blueberry Oatmeal")
 sl.text("🥗 Kale, Spinach & Rocket Smoothie")
 sl.text("🐔 Hard-Boiled Free-Range Egg")
 sl.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
